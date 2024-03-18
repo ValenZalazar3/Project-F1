7c2f248537efcc39de180554bfa03d6ea1094a5e
Valen <valenzalazar3@gmail.com>
2024-03-18 12:35:41 -0300

    Iniciando front

--- server/src/handlers/handlerGetIdDrivers.js
+++ server/src/handlers/handlerGetIdDrivers.js
@@ -1,18 +1,17 @@
 const getIdDrivers = require('../controllers/getIdDrivers')
-
+const getIdDbDrivers = require('../controllers/getIdDbDrivers')
 const handlerGetIdDrivers = async (req, res) => {
   const { id } = req.params
 
   try {
-    const driverId = await getIdDrivers(id)
-
-    if (driverId) {
-      res.status(200).json(driverId)
-    } else {
-      res.status(400).send('No se encontro driver con ese ID')
+    if (id.length < 5) {
+      const driverId = await getIdDrivers(id)
+      return res.status(200).json(driverId)
     }
+    const driver = await getIdDbDrivers(id)
+    res.status(200).json(driver)
   } catch (error) {
-    res.status(404).send({ error: error.message })
+    res.status(404).send({ error: error.message, otro: 'al pasto' })
   }
 }
 
