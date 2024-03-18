057b333c1e0c770863651516c6eed7064469d14d
Valen <valenzalazar3@gmail.com>
2024-03-18 12:35:45 -0300

    Iniciando front

--- server/src/routes/router/routerDrivers.js
+++ server/src/routes/router/routerDrivers.js
@@ -3,11 +3,10 @@ const handlergetAllDrivers = require('../../handlers/handlergetAllDrivers')
 const handlerGetNameDrivers = require('../../handlers/handlerGetNameDrivers')
 const handlerGetIdDrivers = require('../../handlers/handlerGetIdDrivers')
 const handlerPostDrivers = require('../../handlers/handlerPostDrivers')
-const handlerIdDbDrivers = require('../../handlers/handlerIdDbDrivers')
+
 const routerDriver = Router()
 routerDriver.get('/', handlergetAllDrivers)
 routerDriver.get('/name', handlerGetNameDrivers) // peticion http://localhost:3001/drivers/name?name=Lewis
-// routerDriver.get('/:id', handlerGetIdDrivers)
-routerDriver.get('/:id', handlerIdDbDrivers)
+routerDriver.get('/:id', handlerGetIdDrivers)
 routerDriver.post('/', handlerPostDrivers)
 module.exports = routerDriver
